f420d85d3ffcdcbf984082f6eeabcdbcf6714c38
Abhishek Kumar <140600879+Abhishek222747@users.noreply.github.com>
2025-01-01 11:03:00 -0800

    Update Dijkstra_algorithm.cpp

--- Dijkstra_algorithm.cpp
+++ Dijkstra_algorithm.cpp
@@ -1,3 +1,12 @@
+Q.32) Given a weighted, undirected and connected graph where you have given adjacency list adj. 
+You have to find the shortest distance of all the vertices from the source vertex src, and 
+return a list of integers denoting the shortest distance between each node and source vertex src.
+
+Note: The Graph doesn't contain any negative weight edge.
+
+
+
+
 //{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
