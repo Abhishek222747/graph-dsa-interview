c9a9ba1ab163771b25ed4b86749eb8678861d91a
Abhishek Kumar <140600879+Abhishek222747@users.noreply.github.com>
2025-01-01 12:13:00 -0800

    Update Dijkstra_algorithm_set.cpp

--- Dijkstra_algorithm_set.cpp
+++ Dijkstra_algorithm_set.cpp
@@ -1,3 +1,13 @@
+Q.33) Given a weighted, undirected and connected graph where you have given adjacency list adj. 
+You have to find the shortest distance of all the vertices from the source vertex src, and 
+return a list of integers denoting the shortest distance between each node and source vertex src.
+
+Note: The Graph doesn't contain any negative weight edge.
+
+This solution is using set that uses erase technique and save some iteration though no mazor changes 
+in tc comparing to priority queue as erase itself takes log n 
+
+
 //{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
