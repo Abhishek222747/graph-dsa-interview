a5b82ac3a827c011a146aa262234091002cb54d4
Abhishek Kumar <140600879+Abhishek222747@users.noreply.github.com>
2025-01-08 12:51:46 -0800

    Update 43.City with smallest number of neighbours at threshold distance.cpp

--- 43.City with smallest number of neighbours at threshold distance.cpp
+++ 43.City with smallest number of neighbours at threshold distance.cpp
@@ -1,3 +1,13 @@
+There are n cities labeled from 0 to n-1 with m edges connecting them.
+Given the array edges where edges[i] = [fromi , toi ,weighti]  represents a bidirectional 
+and weighted edge between cities fromi and toi, and given the integer distanceThreshold. 
+You need to find out a city with the smallest number of cities that are reachable through some 
+path and whose distance is at most Threshold Distance. If there are multiple such cities, 
+our answer will be the city with the greatest label.
+
+Note: The distance of a path connecting cities i and j is equal to the sum of the edge's weights along that path.
+
+
 //{ Driver Code Starts
 // Initial Template for C++
 
